80d36a77641897179a82d8de02c3d251ac068804
ConanZhang <conankzhang@gmail.com>
2016-10-18 22:11:31 -0600

    Implemented most of pseudo code

--- CS4300_hybrid_agent.m
+++ CS4300_hybrid_agent.m
@@ -29,7 +29,6 @@ persistent t;
 persistent plan;
 persistent safe;
 persistent unvisited;
-
 persistent current;
 
 if isempty(KB)
@@ -40,18 +39,33 @@ if isempty(t)
    t = 0; 
 end
 
+if isempty(safe)
+   safe = zeros(4,4);
+end
+
 if isempty(current)
    current.x = 1;
    current.y = 1; 
    current.r = 0; 
 end
 
+pit_numbers= [1,2,3,4;5,6,7,8;9,10,11,12; 13,14,15,16];
+pno = pit_numbers(current.x, current.y);
 
 CS4300_tell(KB, CS4300_make_percept_sentence(current, percept, t));
-%if CS4300_ask(KB, )
-if CS4300_ask(KB, )
+safe(current.x, current.y) = 1;
+
+% Glitter Ask
+if CS4300_ask(KB, pno + 64)
     plan(end+1) = 4;
+    plan(end+1) = CS4300_plan_route_(current, [1,1,0], safe);
     plan(end+1) = 6;
+end
+
+% Unvisited Ask
+if isempty(plan)
+   unvisited 
+   plan = CS4300_plan_route(current, safe);
 end
 
 % Take Risk
